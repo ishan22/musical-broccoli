d1a63de12cab20c067112e8d0b896b8d5bb87104
Ishan Mitra <imitra@roblox.com>
2025-03-21 15:47:29 -0700

    Add lengthening endpoint and comments

--- main.py
+++ main.py
@@ -14,37 +14,59 @@ class Url(db.Model):
     original_url = mapped_column(String(500), nullable=False)
     shortened_url = mapped_column(String(10), unique=True, nullable=False)
 
+@app.route('/')
+def index():
+    return 'Welcome to URL Shortener!'
+
 @app.route('/shortenUrl', methods=['POST'])
 def shorten_url():
     data = request.get_json()
     long_url = data.get('url', None)
-    print("Request Data: ", data)
+    # TODO: Could add handling for creating custom short tokens
+    # TODO: Should also add handling here for unsafe url types
+        # such as data: or javascript:
     if long_url is None:
         # Bad request
         return jsonify({"error": "url is required"}), 400
     
     existing_url = Url.query.filter_by(original_url=long_url).first()
     if existing_url:
-        return jsonify({"short_url": existing_url.shortened_url})
+        # TODO: IF the user wants a new token, not the old one we currently don't
+        # handle that (for example if they want to replace the old token with a
+        # new custom token)
+        return jsonify({"shortened_url": existing_url.shortened_url}), 200
         # shortened url already exists we can just return that
     
     # generate a new shortened url
-    short_end = ''.join(random.choices(string.ascii_letters + string.digits, k=6))
-    short_url = request.root_url + short_end
-    print(short_url)
-    url_entry = Url(original_url=long_url, shortened_url=short_end)
+    short_token = ''.join(random.choices(string.ascii_letters + string.digits, k=6))
+    # TODO: Possibility duplicate short tokens for different long URLs, would
+    # need to check for uniqueness
+    # Currently we will always just return the first result even if there's a
+    # duplicate
+    short_url = request.root_url + short_token
+    url_entry = Url(original_url=long_url, shortened_url=short_url)
     db.session.add(url_entry)
     db.session.commit()
+    return jsonify({"shortened_url": short_url}), 200
 
-    return jsonify({"shortened_url": short_url})
-
-@app.route('/<short_url>', methods=['GET'])
-def redirect_short_url(short_url):
-    
+@app.route('/<short_token>', methods=['GET'])
+def redirect_short_url(short_token):
+    short_url = request.root_url + short_token
     url_entry = Url.query.filter_by(shortened_url=short_url).first()
     if url_entry is None:
         return jsonify({"error": "url not found"}), 404
     return redirect(url_entry.original_url)
+
+@app.route('/getOriginalUrl', methods=['GET'])
+def get_long_url():
+    data = request.get_json()
+    short_url = data.get('url')
+    if not short_url:
+        return jsonify({"error": "url is required"}), 400
+    url_entry = Url.query.filter_by(shortened_url=short_url).first()
+    if url_entry is None:
+        return jsonify({"error": "url not found"}), 404
+    return jsonify({"original_url" : url_entry.original_url}), 200
 
 
 with app.app_context():
